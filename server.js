7d98adb909d74094009aa9961574516287b807e0
Elinemet Isiyev <elinemet.isiyev@gmail.com>
2021-09-19 22:39:14 +0400

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const { Service, dotenv, Static, Cors, Helmet } = require('@ulvimemmeedov/expressjsplus');
+const path = require('path');
+Service.use(Static(path.join(__dirname, 'views')));
+Service.set('views', '/views');
+Service.use(Cors());
+Service.use(Helmet());
+dotenv.config();
+const port = 3000 || process.env.PORT;
+Service.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/', 'index.html'));
+});
+
+Service.listen(port, () => console.log(`app running ${port}`));
